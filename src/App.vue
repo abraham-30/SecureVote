dff27064f3429fe3a7a7347d8fb6746b8a8eeb76
abraham-30 <128878430+abraham-30@users.noreply.github.com>
2026-01-07 20:45:00 +0700

    added some nice message

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <script setup>
-  import CustomFooter from '@/components/CustomFooter.vue';
-  import CustomNavbar from '@/components/CustomNavbar.vue';
+  // import CustomFooter from '@/components/CustomFooter.vue';
+  // import CustomNavbar from '@/components/CustomNavbar.vue';
+  import { ref } from 'vue';
+
+  const emoji = ref('👋')
+
 </script>
 
 <template>
-  <CustomNavbar/>
-  <CustomFooter/>
+  <!-- <CustomNavbar/> -->
+  <main class="flex items-center justify-center h-screen w-screen">
+    <div class="flex flex-row items-center gap-2 text-6xl">
+      <header>
+        {{ emoji }}
+      </header>
+      <span>
+        Hello World!
+      </span>
+    </div>
+  </main>
+  <!-- <CustomFooter/> -->
 </template>
 
 <style scoped></style>
