e3e8438d78ed5f1400169409b2400a931de155ab
unknown <email@example.com>
2026-06-03 00:20:06 +0700

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -21,9 +21,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  server: {
-    host: "127.0.0.1",
-    port: 5173,
-    hmr: true,
-  },
+  // server: {
+  //   host: "127.0.0.1",
+  //   port: 5173,
+  //   hmr: true,
+  // },
 })
